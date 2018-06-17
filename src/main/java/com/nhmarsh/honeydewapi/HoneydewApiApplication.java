1441df30dca95c2855224cdf95c9d8ee9e7c3ea1
nhmarsh <nhmarsh@gmail.com>
2018-06-17 10:01:43 -0500

    Initial commit

---
+++ src/main/java/com/nhmarsh/honeydewapi/HoneydewApiApplication.java
@@ -0,0 +1,12 @@
+package com.nhmarsh.honeydewapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoneydewApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoneydewApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/nhmarsh/honeydewapi/HoneydewApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nhmarsh.honeydewapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HoneydewApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
