9e5187ef7c765d41bc322bec3d6fc08cea909655
nhmarsh <nhmarsh@gmail.com>
2018-07-28 13:56:46 -0500

    Frontend auth support
    Frontend auth support: Creates simple cors filter to automatically pass OPTIONS requests from the browser.

--- src/main/java/com/nhmarsh/honeydewapi/entity/User.java
+++ src/main/java/com/nhmarsh/honeydewapi/entity/User.java
@@ -45,10 +45,6 @@ public class User {
     @Column(name = "last_name")
     private String lastName;
 
-    /**
-     * Roles are being eagerly loaded here because
-     * they are a fairly small collection of items for this example.
-     */
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "user_role", joinColumns
             = @JoinColumn(name = "user_id",
